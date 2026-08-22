b38e5b9e42739a6bedd9ae0c6860b86165eb33ca
MNL123 <manassentambwa42@gmail.com>
2026-08-22 19:43:05 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,46 @@
-const CACHE_NAME = 'educ-rdc nc-v1';
+const CACHE_NAME = 'educ-rdc-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './logoo.png',
+  './president.jpg',
+  './ministre.jpg',
+  './directeur.jpg',
+  './histoire.jpeg'
 ];
 
-self.addEventListener('install', event => {
+// Installation du Service Worker et mise en cache des fichiers essentiels
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
+      .then((cache) => {
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes réseau (fonctionnement hors-ligne)
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
+      .then((response) => {
+        // Retourne le fichier du cache s'il existe, sinon va le chercher sur le réseau
         return response || fetch(event.request);
       })
   );
 });
-
